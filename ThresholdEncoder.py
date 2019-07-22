d3bc720f084075000736e01328f4326b7a7cf261
Carlos Costa <40147225+ccosta03@users.noreply.github.com>
2019-07-22 12:42:56 +0100

    Initial version

---
+++ ThresholdEncoder.py
@@ -0,0 +1,16 @@
+# Packages
+import pandas as pd
+import tabulate
+
+# Simple encoder for limiting your categorical feature based on threshold %
+# Arguments: dataframe, column, threshold
+# Return: mapping with updated categories
+
+def threshold_encoder(df, column, threshold = 0):
+    perc = pd.DataFrame(df[column].value_counts(True)).reset_index()
+    perc = perc.loc[perc[column]>=threshold,:]
+    perc.columns=[column,'freq']
+    # Optional
+    # Printing values that will be kept
+    print(tabulate(perc, headers=perc.columns, tablefmt='psql'))
+    return dict(zip(perc[column], perc[column]))
